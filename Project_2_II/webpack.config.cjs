27d013456a710d5ba26c0ee1e1196a0606c01d51
Jordan VonBorstel <jordanvonborstel@gmail.com>
2023-04-22 23:08:27 -0700

    s

--- Project_2_II/webpack.config.cjs
+++ Project_2_II/webpack.config.cjs
@@ -48,7 +48,7 @@ module.exports = {
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
-              outputPath: "images/"
+              outputPath: "public/images/"
             }
           }
         ]
